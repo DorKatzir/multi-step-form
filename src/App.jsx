b947a930838208308786216ad42c4ec4a2e88b17
Dor Katzir <katzirdor@gmail.com>
2024-03-01 22:29:30 +0200

    step 1 mobile styles

--- src/App.jsx
+++ src/App.jsx
@@ -617,6 +617,7 @@ function Footer() {
     <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>
         Coded by <a href="#">Dror Katzir</a>.
+		<div className="white-bg"></div>
       </div>
   )
 }
